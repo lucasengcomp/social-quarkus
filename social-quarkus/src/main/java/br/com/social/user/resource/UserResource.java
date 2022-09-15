54cc02a14f1248c6bce60f4fc398600991165428
Lucas Galvao <lucas.engcomp@outlook.com>
2022-09-15 16:22:52 -0300

    :sparkles: Adicionado update de User

--- social-quarkus/src/main/java/br/com/social/user/resource/UserResource.java
+++ social-quarkus/src/main/java/br/com/social/user/resource/UserResource.java
@@ -40,6 +40,20 @@ public class UserResource {
         return Response.status(Response.Status.NOT_FOUND).build();
     }
 
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public Response updateUser(@PathParam("id") Long id, CreateUserRequest userData) {
+        User user = User.findById(id);
+
+        if (user != null) {
+            user.setName(userData.getName());
+            user.setAge(userData.getAge());
+            return Response.ok().build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
     private static User createEntityUser(CreateUserRequest userRequest) {
         User user = new User();
         user.setName(userRequest.getName());
