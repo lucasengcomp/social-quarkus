05109f2a6afda62ecb40524da8f1092cbfc587fb
Lucas Galvao <lucas.engcomp@outlook.com>
2022-09-20 16:46:40 -0300

    :sparkles: `feat` Implementado deletar seguidores

--- social-quarkus/src/main/java/br/com/social/follower/resource/FollowerResource.java
+++ social-quarkus/src/main/java/br/com/social/follower/resource/FollowerResource.java
@@ -80,4 +80,19 @@ public class FollowerResource {
 
         return Response.ok(responseObject).build();
     }
+
+    @DELETE
+    @Transactional
+    public Response unfollowUser(
+            @PathParam("userId") Long userId,
+            @QueryParam("followerId")  Long followerId ){
+        var user = userRepository.findById(userId);
+        if(user == null){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        followerRepository.deleteByFollowerAndUser(followerId, userId);
+
+        return Response.status(Response.Status.NO_CONTENT).build();
+    }
 }
