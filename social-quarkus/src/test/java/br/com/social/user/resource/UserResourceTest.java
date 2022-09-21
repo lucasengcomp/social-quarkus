ed87908d2c622c52f8e81af84e56afb541fafd97
Lucas Galvao <lucas.engcomp@outlook.com>
2022-09-21 08:35:41 -0300

    :white_check_mark: `test` Deve validar um usuário e retornar mensagem de erro quando não forem dados válidos

--- social-quarkus/src/test/java/br/com/social/user/resource/UserResourceTest.java
+++ social-quarkus/src/test/java/br/com/social/user/resource/UserResourceTest.java
@@ -6,6 +6,10 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Map;
+
+import static br.com.social.core.consts.ConstsStatusCode.UNPROCESSABLE_ENTITY_STATUS;
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -32,5 +36,31 @@ public class UserResourceTest {
 
         assertEquals(201, response.statusCode());
         assertNotNull(response.jsonPath().getString("id"));
+    }
+
+    @Test
+    @DisplayName("Should return error when json is not valid")
+    public void createUserValidationErrorTest() {
+        var user = new CreateUserRequest();
+        user.setAge(null);
+        user.setName(null);
+
+        var response =
+                given()
+                        .contentType(ContentType.JSON)
+                        .body(user)
+                        .when()
+                        .post("/users")
+                        .then()
+                        .extract().response();
+
+        assertEquals(UNPROCESSABLE_ENTITY_STATUS, response.statusCode());
+        assertEquals("Validation error", response.jsonPath().getString("message"));
+
+        List<Map<String, String>> errors = response.jsonPath().getList("errors");
+        assertNotNull(errors.get(0).get("message"));
+        assertNotNull(errors.get(1).get("message"));
+//        assertEquals("Age is required", errors.get(0).get("message"));
+//        assertEquals("Name is required", errors.get(1).get("message"));
     }
 }
